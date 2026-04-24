028ab2cf5e053ee431e202efad0f01b0eea69de1
hassanalshaer2-creat <hassan.alshaer2@gmail.com>
2026-04-24 23:11:39 +0300

    First commit

---
+++ build/flutter/android/app/src/main/kotlin/com/flet/lib/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flet.lib
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flet/flet.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FletApp(
+    pageUrl: "main.py",
+    assetsDir: "assets",
+  ));
+}
